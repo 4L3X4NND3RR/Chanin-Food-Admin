60ca72548af5c2a19ad45432db76f112095db55c
alexannderr9000 <alexannderr9000@gmail.com>
2020-05-29 15:23:10 -0600

    Roting y login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppComponent } from './app.component';
 import { MenuAdminComponent } from './components/menu-admin/menu-admin.component';
@@ -7,6 +18,22 @@ import { InicioComponent } from './components/inicio/inicio.component';
 import { MenuRestauranteComponent } from './components/menu-restaurante/menu-restaurante.component';
 import { PlatillosComponent } from './components/platillos/platillos.component';
 import { PedidosComponent } from './components/pedidos/pedidos.component';
+import { LoginRestauranteComponent } from './components/login-restaurante/login-restaurante.component';
+import { AuthGuardService } from './services/auth-guard.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+const routes: Routes = [
+  { path: 'administracion', component: MenuAdminComponent, children: [
+    { path: 'inicio', component: InicioComponent },
+    { path: 'pedidos', component: PedidosComponent },
+    { path: 'menu', component: MenuRestauranteComponent },
+    { path: 'platillos', component: PlatillosComponent },
+    { path: '', component: InicioComponent }
+  ], canActivate: [AuthGuardService] },
+  { path: 'login' , component: LoginRestauranteComponent },
+  { path: '', redirectTo: '/administracion', pathMatch: 'full' },
+  { path: '**', redirectTo: '/administracion', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
@@ -15,10 +42,22 @@ import { PedidosComponent } from './components/pedidos/pedidos.component';
     InicioComponent,
     MenuRestauranteComponent,
     PlatillosComponent,
-    PedidosComponent
+    PedidosComponent,
+    LoginRestauranteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' }),
+    ReactiveFormsModule,
+    MatInputModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/menu-admin/menu-admin.component.ts
+++ src/app/components/menu-admin/menu-admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { AutenticacionService } from 'src/app/services/autenticacion.service';
 
 @Component({
   selector: 'app-menu-admin',
@@ -6,10 +10,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-admin.component.css']
 })
 export class MenuAdminComponent implements OnInit {
+  isAuthenticated: boolean;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private breakpointObserver: BreakpointObserver, public authService: AutenticacionService) { 
+    this.authService.isAuthenticated.subscribe(
+      (isAuthenticated: boolean) => this.isAuthenticated = isAuthenticated
+    );
   }
 
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(result => result.matches),
+    shareReplay()
+  );
+
+  async ngOnInit() {
+    this.isAuthenticated = await this.authService.checkAuthenticated();
+  }
+
+  logout() {
+    this.authService.logout('/');
+  }
 }
